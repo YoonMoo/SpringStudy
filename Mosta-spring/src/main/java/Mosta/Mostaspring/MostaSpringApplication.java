06ed960f1d23bb7b82bb28bbc21d64ccea39cc95
Yoonmo <momooy6929@gmail.com>
2022-11-01 22:17:23 +0900

    #프로젝트생성

---
+++ Mosta-spring/src/main/java/Mosta/Mostaspring/MostaSpringApplication.java
@@ -0,0 +1,13 @@
+package Mosta.Mostaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MostaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MostaSpringApplication.class, args);
+	}
+
+}
---
+++ Mosta-spring/src/test/java/Mosta/Mostaspring/MostaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Mosta.Mostaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MostaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
