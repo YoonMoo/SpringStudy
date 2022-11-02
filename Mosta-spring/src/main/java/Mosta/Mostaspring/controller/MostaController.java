5bad61b2ca405b85eaa5d12e5b5e05467bf96838
Yoonmo <momooy6929@gmail.com>
2022-11-02 22:06:35 +0900

    정적 컨텐츠 / MVC와 템플릿 엔진

--- Mosta-spring/src/main/java/Mosta/Mostaspring/controller/MostaController.java
+++ Mosta-spring/src/main/java/Mosta/Mostaspring/controller/MostaController.java
@@ -3,6 +3,7 @@ package Mosta.Mostaspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MostaController {
@@ -11,5 +12,11 @@ public class MostaController {
     public String hello(Model model){
         model.addAttribute("data", "hello!!");
         return "hello"; // html 파일을 찾아간다.
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
