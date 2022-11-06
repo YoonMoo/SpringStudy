f4204abdc2e5d9b44e786c45f7b2b5df66cbced2
Yoonmo <momooy6929@gmail.com>
2022-11-06 15:23:22 +0900

    회원 웹 기능 - 홈 화면 추가

--- Mosta-spring/src/main/java/Mosta/Mostaspring/controller/MemberController.java
+++ Mosta-spring/src/main/java/Mosta/Mostaspring/controller/MemberController.java
@@ -3,6 +3,7 @@ package Mosta.Mostaspring.controller;
 import Mosta.Mostaspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -17,5 +18,10 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
     }
 }
